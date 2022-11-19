69edc1659c7a828e33b0885839bde44bcbdd4414
sun00120 <sun00120@algonquinlive.com>
2022-11-19 03:18:54 -0500

    add initial home &Team screens

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./screens/HomeScreen";
+import TeamScreen from "./screens/TeamScreen";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Hello World!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ focused, size, color }) => (
+                <FontAwesome name={"home"} size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Team"
+            component={TeamScreen}
+            options={{
+              tabBarIcon: ({ focused, size, color }) => (
+                <Ionicons name={"people"} size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
@@ -17,7 +43,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
